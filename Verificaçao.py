d60fc75193f90efcc290429c3af83a86add6deb5
Arthur Castro <96489620+ArthurALDC@users.noreply.github.com>
2024-05-17 22:00:56 -0300

    Delete Verificaçao.py

--- Verificaçao.py
+++
@@ -1,5 +0,0 @@
-class Livro():
-    def __init__(self, titulo, isbn):
-        self.titulo =  titulo
-        self.isbn = isbn 
-        print("Construtor chamado para criar um objeto dessa classe")
